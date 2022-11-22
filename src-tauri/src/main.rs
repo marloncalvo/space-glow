f052a925ba71b8e20be662717cc96e3ed174e7f0
Marlon Calvo <marloncalv98@gmail.com>
2022-11-22 00:50:46 -0800

    anotha

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,8 +12,8 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    // tauri::Builder::default()
+    //     .invoke_handler(tauri::generate_handler![greet])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
 }
