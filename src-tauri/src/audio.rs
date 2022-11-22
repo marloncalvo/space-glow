f51d1e9c7c086761e55f37897fb816fa44ea9812
Marlon Calvo <marloncalv98@gmail.com>
2022-11-22 00:50:03 -0800

    clutch changes

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,5 +1,5 @@
 use core::time;
-use std::f32::consts::PI;
+use std::{f32::consts::PI};
 
 fn play() {
     use cpal::traits::HostTrait;
@@ -20,16 +20,33 @@ fn play() {
     use std::sync::atomic::{AtomicUsize, Ordering};
     let lock = Arc::new(Mutex::new(0));
     let numberOfSamples = 500;
+    let config = supported_config.config();
+    let freq = 400;
+    let freq_f64 = freq as f64;
+    let sample_rate_f64 = config.sample_rate.0 as f64;
+    let quot = ((1.0/freq_f64)/(1.0/sample_rate_f64)) as u32;
+    let quot_f32 = quot as f32;
+    println!("quot={:?}", quot);
     let stream = device.build_output_stream(
-        &supported_config.config(), 
+        &config, 
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for sample in data.iter_mut() {
+                //Clayton
+                // let mut count = lock.lock().unwrap();
+                // *count += 1
+                // *sample = Sample::<i16>::from()
+
+
+
+
+                // Marlon
                 let mut count = lock.lock().unwrap();
-                let count_f32 = (*count + 1) as f32;
+                let count_u32 = (*count + 1);
                 *count += 1;
-                let amplitude = (400.0 * PI * count_f32).sin() * 0.2;
-                *sample = Sample::from(&0.0);
-                println!("count={:?}, amplitude={:?}", count, amplitude);
+                let inner = (PI * (count_u32 as f32)) / quot_f32;
+                let amplitude = (inner).sin() * 10.0;
+                *sample = Sample::from(&amplitude);
+                println!("count={:?}, amplitude={:?}, inner={:?}", count, amplitude, inner);
             }
         }, 
         move |err| {
@@ -37,7 +54,7 @@ fn play() {
         }).unwrap();
     stream.play().unwrap();
     use std::{thread, time};
-    thread::sleep(time::Duration::from_secs(5));
+    thread::sleep(time::Duration::from_secs(2));
     stream.pause().unwrap();
 }
 
@@ -49,4 +66,11 @@ mod tests {
     fn it_plays() {
         play();
     }
+    fn check_cfg() {
+        
+    }
 }
+
+// f = 400Hz -> T = 1/f -> T = 1/400. Cycle takes 0.0025 seconds.
+// sample_rate is 48KHz. Cycle takes 1/48K. Need 120 samples for one 400Hz cycle.
+// So, we need (1/f)/(1/sample_rate)
