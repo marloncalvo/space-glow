6548e87c043a34e8880e78bc98fe30c53401bd5f
Marlon Calvo <marloncalv98@gmail.com>
2022-11-23 02:26:00 -0800

    trash stuff

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,12 +1,69 @@
-use core::time;
+// use core::time;
 use std::{f32::consts::PI};
+use cpal::traits::HostTrait;
+use cpal::{Data, Sample, SampleFormat};
+use cpal::traits::{DeviceTrait};
+use cpal::traits::{StreamTrait};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{thread, time};
+
+type FuncT = dyn Fn(f32) -> f32;
+
+struct Sine<'a> {
+    amplitude: &'a FuncT,
+    omega: &'a FuncT,
+    phi: &'a FuncT,
+    alpha: &'a FuncT,
+}
+
+// https://www.quora.com/Is-there-a-formula-to-add-two-sine-waves-with-different-amplitudes-different-periods-and-different-phase-shifts
+
+impl Sine<'_> {
+    fn add(wave_0: Self, wave_1: Self) -> Self {
+        // Usage: Sine::add(wave1, wave2)
+        let w_add = |t: f32| -> f32 {(wave_0.omega)(t) + (wave_1.omega)(t)};
+        let w_sub = |t: f32| -> f32 {(wave_0.omega)(t) - (wave_1.omega)(t)};
+        
+        let phi_add = |t: f32| -> f32 {(wave_0.phi)(t) + (wave_1.phi)(t)};
+        let phi_sub = |t: f32| -> f32 {(wave_0.phi)(t) - (wave_1.phi)(t)};
+
+        let a_add = |t: f32| -> f32 {(wave_0.amplitude)(t) + (wave_1.amplitude)(t)};
+
+        let a = |t: f32| -> f32 {
+            (wave_0.amplitude)(t).powi(2) +
+            (wave_1.amplitude)(t).powi(2) +
+            (2.0 * (wave_0.amplitude)(t) * (wave_1.amplitude)(t)) * 
+            (((w_sub)(t)*t)+(phi_sub)(t)).cos()
+        };
+
+        let big_phi = |t: f32| -> f32 {
+            (
+                (((wave_0.amplitude)(t) - (wave_1.amplitude)(t)) / ((wave_0.amplitude)(t) + (wave_1.amplitude)(t))) *
+                (((w_sub)(t)*t/2.0)+((phi_sub)(t)/2.0)).tan()
+            ).tanh()
+        };
+
+        let omega = |t: f32| -> f32 {(w_add)(t)*t/2.0};
+        let phi = |t: f32| -> f32 {(phi_add)(t)/2.0 + (big_phi)(t)};
+        let alpha = |t: f32| -> f32 {(wave_0.alpha)(t) + (wave_1.alpha)(t)};
+
+        return Sine{
+            amplitude: &a,
+            omega: &omega,
+            phi: &phi,
+            alpha: &alpha
+        };
+    }
+
+    fn subtract(wave1: Self, wave2: Self) -> Self {
+        unimplemented!()
+    }
+}
 
 fn play() {
-    use cpal::traits::HostTrait;
-    use cpal::{Data, Sample, SampleFormat};
     let host = cpal::default_host();
     let device = host.default_output_device().expect("no output device available");
-    use cpal::traits::{DeviceTrait};
     let mut supported_configs_range = device
         .supported_output_configs()
         .expect("error while querying configs");
@@ -15,45 +72,26 @@ fn play() {
         .expect("no supported configs")
         .with_max_sample_rate();
     println!("{:?}", supported_config);
-    use cpal::traits::{StreamTrait};
-    use std::sync::{Arc, Mutex};
-    use std::sync::atomic::{AtomicUsize, Ordering};
-    let lock = Arc::new(Mutex::new(0));
-    let numberOfSamples = 500;
     let config = supported_config.config();
-    let freq = 400;
-    let freq_f64 = freq as f64;
-    let sample_rate_f64 = config.sample_rate.0 as f64;
-    let quot = ((1.0/freq_f64)/(1.0/sample_rate_f64)) as u32;
-    let quot_f32 = quot as f32;
+    let freq: f32 = 400 as f32;
+    let sample_rate: f32 = config.sample_rate.0 as f32;
+    let quot: f32 = sample_rate / (freq * PI);
+    let mut count: u32 = 0;
     println!("quot={:?}", quot);
     let stream = device.build_output_stream(
         &config, 
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for sample in data.iter_mut() {
-                //Clayton
-                // let mut count = lock.lock().unwrap();
-                // *count += 1
-                // *sample = Sample::<i16>::from()
-
-
-
-
-                // Marlon
-                let mut count = lock.lock().unwrap();
-                let count_u32 = (*count + 1);
-                *count += 1;
-                let inner = (PI * (count_u32 as f32)) / quot_f32;
-                let amplitude = (inner).sin() * 10.0;
-                *sample = Sample::from(&amplitude);
-                println!("count={:?}, amplitude={:?}, inner={:?}", count, amplitude, inner);
+                count += 1;
+                let amp: f32 = (count as f32 / quot).sin();
+                *sample = Sample::from(&amp);
+                // println!("count={:?}, inner={:?}, other_count={:?}", count, inner, (count as f32) / quot_f32);
             }
         }, 
         move |err| {
             eprintln!("an error occurred on the output audio stream: {}", err);
         }).unwrap();
     stream.play().unwrap();
-    use std::{thread, time};
     thread::sleep(time::Duration::from_secs(2));
     stream.pause().unwrap();
 }
