7a2d638c97bfddab9820d84763a30c691e8d7bdf
CABrinegar <claytonbrinegar@gmail.com>
2022-11-23 19:00:53 +0900

    clean up & simplify

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,60 +1,38 @@
-use core::time;
-use std::{f32::consts::PI};
+#![allow(unused_imports)]
+#![allow(dead_code)]
+// use core::time;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Data, Sample, SampleFormat};
+use std::f32::consts::PI;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+use std::{thread, time};
 
 fn play() {
-    use cpal::traits::HostTrait;
-    use cpal::{Data, Sample, SampleFormat};
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("no output device available");
-    use cpal::traits::{DeviceTrait};
-    let mut supported_configs_range = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let supported_config = supported_configs_range
-        .next()
-        .expect("no supported configs")
-        .with_max_sample_rate();
-    println!("{:?}", supported_config);
-    use cpal::traits::{StreamTrait};
-    use std::sync::{Arc, Mutex};
-    use std::sync::atomic::{AtomicUsize, Ordering};
-    let lock = Arc::new(Mutex::new(0));
-    let numberOfSamples = 500;
-    let config = supported_config.config();
-    let freq = 400;
-    let freq_f64 = freq as f64;
-    let sample_rate_f64 = config.sample_rate.0 as f64;
-    let quot = ((1.0/freq_f64)/(1.0/sample_rate_f64)) as u32;
-    let quot_f32 = quot as f32;
-    println!("quot={:?}", quot);
-    let stream = device.build_output_stream(
-        &config, 
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            for sample in data.iter_mut() {
-                //Clayton
-                // let mut count = lock.lock().unwrap();
-                // *count += 1
-                // *sample = Sample::<i16>::from()
-
-
-
-
-                // Marlon
-                let mut count = lock.lock().unwrap();
-                let count_u32 = (*count + 1);
-                *count += 1;
-                let inner = (PI * (count_u32 as f32)) / quot_f32;
-                let amplitude = (inner).sin() * 10.0;
-                *sample = Sample::from(&amplitude);
-                println!("count={:?}, amplitude={:?}, inner={:?}", count, amplitude, inner);
-            }
-        }, 
-        move |err| {
-            eprintln!("an error occurred on the output audio stream: {}", err);
-        }).unwrap();
+    let (dev, cfg) = supported_cfgs();
+    let f1: f32 = 400.0; // frequency 1
+                         // let f2: f32 = 300.0;
+    let sample_rate: f32 = cfg.sample_rate.0 as f32;
+    let q1 = sample_rate / (f1 * PI);
+    let mut count: u32 = 0;
+    let stream = dev
+        .build_output_stream(
+            &cfg,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                for sample in data.iter_mut() {
+                    count += 1;
+                    let amp: f32 = (count as f32 / q1).sin();
+                    *sample = Sample::from(&amp);
+                    // println!("count={:?}, amplitude={:?}", count, amp);
+                }
+            },
+            move |err| {
+                eprintln!("an error occurred on the output audio stream: {}", err);
+            },
+        )
+        .unwrap();
     stream.play().unwrap();
-    use std::{thread, time};
-    thread::sleep(time::Duration::from_secs(2));
+    thread::sleep(time::Duration::from_millis(500));
     stream.pause().unwrap();
 }
 
@@ -66,9 +44,22 @@ mod tests {
     fn it_plays() {
         play();
     }
-    fn check_cfg() {
-        
-    }
+}
+
+fn supported_cfgs() -> (cpal::Device, cpal::StreamConfig) {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+    let mut supported_cfgs = device
+        .supported_output_configs()
+        .expect("error while querying configs");
+    let cfg = supported_cfgs
+        .next()
+        .expect("no supported configs")
+        .with_max_sample_rate()
+        .config();
+    (device, cfg)
 }
 
 // f = 400Hz -> T = 1/f -> T = 1/400. Cycle takes 0.0025 seconds.
