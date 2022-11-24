3bad75e85ce0eb2ebe2ea6cb04ff6f5b231d277e
CABrinegar <claytonbrinegar@gmail.com>
2022-11-24 16:42:03 +0900

    simple sine wave addition

--- src-tauri/src/audio.rs
+++ src-tauri/src/audio.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
 // use core::time;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{Data, Sample, SampleFormat};
@@ -7,6 +9,75 @@ use std::f32::consts::PI;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
+
+struct Sine {
+    amp: f32,
+    hz: f32,
+    phase: f32, // in radians
+}
+
+impl Sine {
+    fn default() -> Self {
+        Self {
+            amp: 1.0,
+            hz: 440.0,
+            phase: 0.0,
+        }
+    }
+}
+
+struct ComplexSine {
+    sines: Vec<Sine>,
+}
+
+impl ComplexSine {
+    fn new() -> Self {
+        Self { sines: vec![] }
+    }
+
+    fn add(&mut self, sine: Sine) {
+        self.sines.push(sine);
+    }
+
+    fn sample(&self, rads: f32) -> f32 {
+        let mut res_amp = 0.0;
+        for sine in &self.sines {
+            res_amp += sine.amp * (PI * sine.hz * rads + sine.phase).sin();
+        }
+        res_amp /= self.sines.len() as f32;
+        res_amp
+    }
+}
+fn play2() {
+    let (dev, cfg) = supported_cfgs();
+    let mut cs = ComplexSine::new();
+    cs.add(Sine::default());
+    cs.add(Sine {
+        hz: 300.0,
+        ..Sine::default()
+    });
+    let mut count: f32 = 0.0;
+    let stream = dev
+        .build_output_stream(
+            &cfg,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                for sample in data.iter_mut() {
+                    count += 1.0;
+                    let rads = (count % cfg.sample_rate.0 as f32) / cfg.sample_rate.0 as f32;
+                    let amp = cs.sample(rads);
+                    *sample = Sample::from(&amp);
+                    // println!("count={:?}, amplitude={:?}", count, amp);
+                }
+            },
+            move |err| {
+                eprintln!("an error occurred on the output audio stream: {}", err);
+            },
+        )
+        .unwrap();
+    stream.play().unwrap();
+    thread::sleep(time::Duration::from_millis(500));
+    stream.pause().unwrap();
+}
 
 fn play() {
     let (dev, cfg) = supported_cfgs();
@@ -23,7 +94,7 @@ fn play() {
                     count += 1;
                     let amp: f32 = (count as f32 / q1).sin();
                     *sample = Sample::from(&amp);
-                    // println!("count={:?}, amplitude={:?}", count, amp);
+                    println!("count={:?}, amplitude={:?}", count, amp);
                 }
             },
             move |err| {
@@ -43,6 +114,11 @@ mod tests {
     #[test]
     fn it_plays() {
         play();
+    }
+
+    #[test]
+    fn it_plays2() {
+        play2();
     }
 }
 
